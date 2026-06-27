c3a348d5baa7d99bfa1216046aec3351ad7a6a78
Oleg Pustovit <me@opustovit.com>
2026-06-27 11:59:32 +0200

    feat: update

--- container/home.nix
+++ container/home.nix
@@ -1,6 +1,8 @@
 { ... }:
 
 {
+  snowbear.git.setIdentity = false;
+
   home.sessionVariables = {
     SNOWBEAR_CONTAINER = "1";
   };
--- home/git.nix
+++ home/git.nix
@@ -2,14 +2,23 @@
 { config, lib, pkgs, ... }:
 
 {
-  programs.git = {
-    enable = true;
-    lfs.enable = true;
-    settings = {
-      user.name = "Oleg Pustovit";
-      user.email = "me@opustovit.com";
-      core.editor = "nvim";
-      push.autoSetupRemote = true;
+  options.snowbear.git.setIdentity = lib.mkOption {
+    type = lib.types.bool;
+    default = builtins.getEnv "SNOWBEAR_HOME_CONTAINER" != "1";
+    description = "Whether Home Manager should write Git user.name and user.email.";
+  };
+
+  config = {
+    programs.git = {
+      enable = true;
+      lfs.enable = true;
+      settings = {
+        core.editor = "nvim";
+        push.autoSetupRemote = true;
+      } // lib.optionalAttrs config.snowbear.git.setIdentity {
+        user.name = "Oleg Pustovit";
+        user.email = "me@opustovit.com";
+      };
     };
   };
 }
